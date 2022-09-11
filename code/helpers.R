fc4d8432838988d7d26c5e0c8640d70242a1f618
jdberson <jacob.berson@uwa.edu.au>
2022-09-11 21:38:17 +0800

    commit to restart gh actions

--- code/helpers.R
+++ code/helpers.R
@@ -5,3 +5,8 @@ library("usethis")
 
 # Note - need to make at least one local commit before running this line
 use_github()
+
+# Try using renv::snapshot() to overcome publishing issue of rmarkdown not being found
+renv::snapshot() # writes a lock file
+
+# Final shot
